caeffdbdc7b0942a2f65c601192cfd50e218824c
Lev Savolsky <lev.savolsky@gmail.com>
2021-04-15 11:19:24 +0300

    Придумал, как цвета веткам придавать, вроде

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <vector>
+#include <map>
+
 
 int main() {
+  std::map<std::vector<int>, std::vector<int>> metro(22);
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
