7d3d79c295a445250f7bf69663fae7e318e06db2
Lev Savolsky <lev.savolsky@gmail.com>
2021-04-15 11:43:21 +0300

    Удалил хелло ворлд

--- main.cpp
+++ main.cpp
@@ -7,6 +7,5 @@ int main() {
   //Это просто пример, но на самом деле надо додумать немного, потому что писать всё время всю пару это немного запарно
   std::map<std::pair<int, int>, std::vector<int>> metro = { {{1, 2}, {1, 2, 3}}, {{2, 2}, {1, 2, 3}} };
   std::cout << metro[{1, 2}][1];
-  std::cout << "Hello, World!" << std::endl;
   return 0;
 }
